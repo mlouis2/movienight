2419a7bee3f6c26e6519d5db96cbb7acf593ca44
Leigh Lewis <leighlewis@Leighs-MacBook-Pro.local>
2017-08-15 15:21:40 -0700

    fade in is working

---
+++ javascript/common.js
@@ -0,0 +1,44 @@
+var images = ['#one', '#two','#three','#four','#five','#six','#seven']
+imageIndex = 0
+currentImage = $('#one')
+fadeDuration = 3000
+
+
+
+$(window).load(function() {
+		$('.photo').hide();
+     $(".photo").each(function(i) {
+          $(this).delay(400*i).fadeIn(1000);
+     });
+});
+
+// $(document).ready(function() {
+//
+//     $('.photo').hide();
+//     $('#one').each(function(i) {
+//         if (this.complete) {
+//             $('#one').fadeIn();
+//         } else {
+//             $(this).load(function() {
+//                 $('#one').fadeIn(4000);
+//             });
+//         }
+//     });
+// });
+//
+// $(document).ready(function() {
+//
+//     $('.photo').hide();
+//     $('#two').each(function() {
+//         if (this.complete) {
+// 					setTimeout(3000);
+//           $('#two').fadeIn(4000);
+//         }
+// 				else {
+//             $(this).load(function() {
+// 							setTimeout(3000);
+//             		$('#two').fadeIn(4000);
+//             });
+//         }
+//     });
+// });
